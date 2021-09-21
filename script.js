ce138d04fdf7485d4cd799bd2447c0b9c54ffd54
laxmikanthkonthum <laxmikanthkonthum@gmail.com>
2021-09-22 04:32:04 +0530

    Fix: corrected logic

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const dob = document.querySelector(".date-input");
 const luckyNo = document.querySelector(".number-input");
 const submitBtn = document.querySelector(".submit-button");
-const result = document.querySelector(".output")
-const result1 = document.querySelector(".output1")
-var val =  dob.value.replaceAll('-','')
-var dateInput = Number(val);
-var numberInput = Number(luckyNo.value)
+const result = document.querySelector(".output");
+const result1 = document.querySelector(".output1");
+// var val =  dob.value.replaceAll('-','');
+// var dateInput = Number(val);
+// var numberInput = Number(luckyNo.value);
 submitBtn.addEventListener('click',function logic(){
    if (dob.value == ""){
       validation();
   }
   else{
+    var numberInput = Number(luckyNo.value)
+    console.log(numberInput, typeof numberInput)
     if(numberInput < 1){
       result.innerText = "Please enter a lucky number greater than 1"
     }else{
+      var val =  dob.value.replaceAll('-','');
+      var dateInput = Number(val);
+  console.log(dateInput,val);
   var sum = 0;
   while(dateInput > 0){
   var rem = dateInput % 10;
   sum = sum + rem;
   dateInput = Math.trunc(dateInput/10)
   }
+  console.log(sum,numberInput)
   luckChecker(sum,numberInput);
 }
   }
@@ -36,7 +42,9 @@ function validation(){
     }
     }
 }
+
 function luckChecker(sum,numberInput){
+  console.log(sum,numberInput)
     if (sum % numberInput == 0){
         result.innerText  = "Yay! Your Birthday is Lucky"
     }
